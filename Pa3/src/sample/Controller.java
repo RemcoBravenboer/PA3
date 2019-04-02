0a24c50dc8c2da4cb5d673a9ba2af24640c7a50d
bravenr001 <r.bravenboer1996@gmail.com>
2019-04-02 11:34:31 +0200

    Added step1 layout and button click event in controller

--- Pa3/src/sample/Controller.java
+++ Pa3/src/sample/Controller.java
@@ -1,4 +1,13 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    private TextField nVal;
+
+    public void qpCalc(){
+        int nValue = Integer.parseInt(nVal.getText());
+    }
 }
